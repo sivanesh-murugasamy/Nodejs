4b0c59f7f983751594a81f67ecf233b6c99a56fd
sivanesh <sivaneshm.21msc@kongu.edu>
2024-10-02 14:12:45 +0530

    afternoon

---
+++ LocalModules/function.js
@@ -0,0 +1,16 @@
+
+function add(num1,num2)
+{
+    return `The Addition is ${num1+num2}`;
+}
+
+function sub(num1,num2)
+{
+    return `The Subtraction of ${num1-num2}`;
+}
+function mul(num1,num2)
+{
+    return `The Multiplication of ${num1*num2}`;
+}
+
+module.exports={add,sub,mul};
---
+++ LocalModules/other.js
@@ -0,0 +1,5 @@
+const fun=require('./function');
+
+console.log(fun.add(4,3));
+console.log(fun.sub(7,6));
+console.log(fun.mul(2,3));
