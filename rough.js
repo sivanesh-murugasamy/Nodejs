fbb255e9a6c9d34205ad9df4f9566ec794c07ea3
sivanesh-murugasamy <88439516+sivanesh-murugasamy@users.noreply.github.com>
2024-12-26 23:40:13 +0530

    diff

--- rough.js
+++ rough.js
@@ -1,14 +1,28 @@
 const mongoose=require('mongoose');
 
 const connection_String='mongodb://localhost:27017/Datastore';
-mongoose.connect(connection_String, {
+const conn = mongoose.createConnection(connection_String, {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-   
-})
-.then(()=>{
+});
+conn.once('open',()=>{
     console.log("Succesfully Connected to Mongodb");
-})
-.catch((error)=>{
-    console.log(error);
+
+   
+
+    const model=mongoose.model('user',schema);
+
+    const newUser=new model({
+        name:"rajesh",
+        email:"raja@gmail.com",
+        age:20
+    });
+
+    newUser.save()
+       .then(()=>{
+         console.log("User Saved Successfully");
+       })
+       .catch((err)=>{
+          console.log(err);
+       })
+    });
 });
